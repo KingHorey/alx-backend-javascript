f665c7547c19af7f7ddb7cb5a82c68c0bf4f07e6
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 03:12:42 +0000

    add setter, getter and thorw error on failure to holbertonCOurse class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof (value) === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(value) {
+    if (typeof (value) === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(value) {
+    if (typeof (value) === 'object') {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an arry of strings');
+    }
+  }
+}
