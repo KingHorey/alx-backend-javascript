0be9253482599a3bace111a6bd62bc7424b42f24
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 23:44:05 +0000

    remove comma operator in constructor

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,34 @@
 export default class Currency {
   /* eslint no-underscore-dangle: ["error", {"allowAfterThis": true}] */
   constructor(code, name) {
-    this._code = code,
-    this._name = name
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
-	return this._code;
+    return this._code;
   }
+
   get name() {
-	return this._name;
+    return this._name;
   }
+
   set name(value) {
-	if (typeof (value) === 'string') {
-        this._name = value;
-	}
-	else {
-		throw new TypeError("Name must be a string");
-	}
+    if (typeof (value) === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
+
   set code(value) {
-	if (typeof (value) === 'string') {
-        this._code = value
-	}
-	else {
-		throw new TypeError("Code must be a string")
-	}
+    if (typeof (value) === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
   }
+
   displayFullCurrency() {
     return (`${this._name} ${this._code}`);
   }
