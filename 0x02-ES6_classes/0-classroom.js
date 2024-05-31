dd21a9d33715e27bfa132baf7ce9c353cb51526a
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 02:13:10 +0000

    fix type in property name

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,6 +2,6 @@ export default class ClassRoom {
   /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
   /* eslint no-unused-vars: ["error", { "args": "none" }] */
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
