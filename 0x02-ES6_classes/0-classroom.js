5dac000d6406ff36b8d8f4fd2c0e52cdd85b690a
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 01:49:02 +0000

    add es-lint exceptions

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,7 @@
 export default class ClassRoom {
+  /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "maxStuden*" }] */
   constructor(maxStudentsSize) {
+    /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
     this._maxStudentsSize = maxStudentSize;
   }
 }
