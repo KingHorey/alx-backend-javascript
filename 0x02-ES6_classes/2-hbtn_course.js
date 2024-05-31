bdb4f01cc518086319110ce21d8af8cbdb8c5002
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 04:21:48 +0100

    add type checks during instance creation

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,21 @@
 export default class HolbertonCourse {
   /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    if (!students.isArray || students.every(x => typeof (x) === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an arry of strings');
+    }
   }
 
   get name() {
