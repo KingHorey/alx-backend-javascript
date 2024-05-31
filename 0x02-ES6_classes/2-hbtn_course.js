8c919210b376191b1bb024fa5773c9bf75848a00
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-05-31 04:28:11 +0000

    fix error in class constructor check

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -11,7 +11,7 @@ export default class HolbertonCourse {
     } else {
       throw new TypeError('Length must be a number');
     }
-    if (!students.isArray || students.every((x) => typeof (x) === 'string')) {
+    if (!Array.isArray(students) || !students.every((x) => typeof (x) === 'string')) {
       this._students = students;
     } else {
       throw new TypeError('Students must be an array of strings');
