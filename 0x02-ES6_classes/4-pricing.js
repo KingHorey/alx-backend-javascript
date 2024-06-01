d3519aab5449aa02a8f5b7c3efc6f5bf44d66954
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-01 00:33:26 +0000

    add static to convertPrice and general return statement in same method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,40 +1,45 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
-	/* eslint no-underscore-dangle: ['error', { 'allowAfterThis': true }] */
+  /* eslint no-underscore-dangle: ['error', { 'allowAfterThis': true }] */
   constructor(amount, currency) {
-	this._amount = amount;
-	this._currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
-	return this._amount;
+    return this._amount;
   }
+
   get currency() {
-	return this._currency;
+    return this._currency;
   }
+
   set amount(value) {
-	if (typeof (value) === 'number') {
-		this._amount = value
-	}
-	else {
-		throw new TypeError("Amount must be a number");
-	}
+    if (typeof (value) === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
   }
+
   set currency(value) {
-	if (value instanceof Currency) {
-		this._currency = value;
-	}
-	else {
-		throw new TypeError("Currency must be a string");
-	}
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a string');
+    }
   }
+
   displayFullPrice() {
-	return (`${this._amount} ${this._currency.name} (${this._currency.code})`);
+    return (`${this._amount} ${this._currency.name} (${this._currency.code})`);
   }
-  convertPrice(amount, conversionRate) {
-	if ((typeof (amount) === 'number') && (typeof (conversionRate) === 'number')) {
-		return (amount * conversionRate);
-	}
+
+  static convertPrice(amount, conversionRate) {
+    if ((typeof (amount) === 'number') && (typeof (conversionRate) === 'number')) {
+      return (amount * conversionRate);
+    }
+
+    throw new TypeError('Number and ConversionRate should be numbers');
   }
 }
