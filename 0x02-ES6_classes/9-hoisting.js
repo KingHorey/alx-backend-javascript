76217835697ba5067f4a10d1d6b06ed643cdd934
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-01 01:53:33 +0000

    add eslint rules

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,7 @@
+/* eslint no-underscore-dangle: ["error", {"allowAfterThis": true }] */
+/* eslint no-unused-vars: ["error", {"allow": none }] */
+/* eslint max-classes-per-file: ["error", 2] */
+
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -42,5 +46,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-var listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
