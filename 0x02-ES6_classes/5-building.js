108d4bbc7ae80fe2ab9a818b8ee5bef9a63ab7c7
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-01 02:37:44 +0000

    fix lint errors

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,21 @@
 export default class Building {
   /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
   constructor(sqft) {
-	this._sqft = sqft;
-	if ((this.constructor !== Building) && (this.evacuationWarningMessage === undefined)) {
-		throw new Error("Class extending Building must override evacuationWarningMessage");
-	}
-  }
-  set sqft(value) {
-	if (typeof (value) === 'number') {
-		this._sqft = value;
-	}
-	else {
-		throw new TypeError("Sqft should be a number");
-	}
-  }
-  get sqft() {
-	return this._sqft;
+    this._sqft = sqft;
+    if ((this.constructor !== Building) && (this.evacuationWarningMessage === undefined)) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
+  set sqft(value) {
+    if (typeof (value) === 'number') {
+      this._sqft = value;
+    } else {
+      throw new TypeError('Sqft should be a number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
 }
