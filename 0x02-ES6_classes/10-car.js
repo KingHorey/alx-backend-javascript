635498c2f2f3f9fece8fa0102c021bd212714943
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-02 02:18:52 +0100

    create instances from derived class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,18 @@
+/* eslint no-underscore-dangle: ["error", {"allowAfterThis": true }] */
+
 export default class Car {
-  /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
   constructor(brand, motor, color) {
-	this._brand = brand;
-	this._motor = motor;
-	this._color = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
+
+  static get [Symbol.species]() {
+    /* returns the child of the parent class */
+    return this;
+  }
+
   cloneCar() {
-	return new Car;
+    return new this.constructor();
   }
 }
