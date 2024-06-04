a318be6f1d8aaa9a7bab3a774cbc1b94d5cb5d48
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-04 17:39:21 +0100

    change conditional syntax

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,7 +5,7 @@ function queryAPI(endpoint) {
     weakMap.set(endpoint, 1);
     return;
   }
-  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 4)) {
+  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 5)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 
   } else {
