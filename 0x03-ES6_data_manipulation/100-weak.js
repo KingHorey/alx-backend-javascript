d19d6b75b7f410981ed4f0cceab3ecffef902488
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-04 17:44:31 +0100

    fix weakMap start point

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+    weakMap.set(endpoint, 1);
     return;
   }
   if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 4)) {
