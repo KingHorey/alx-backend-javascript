c3e59ea693095fbb4c0a2ee18dad9e84eefdc3ef
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-04 17:42:55 +0100

    fix eslint errors

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,15 +2,13 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
+    weakMap.set(endpoint, 0);
     return;
   }
-  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 5)) {
+  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 4)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-
   } else {
     throw new Error('Endpoint load is high');
   }
 }
-// console.log(weakMap)
 module.exports = { queryAPI, weakMap };
