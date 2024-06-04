bee165688b4991acb0b2de0c0b15a71ee54e49dc
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-04 16:44:58 +0100

    count number of api to weakMap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  if ((weakMap.has(endpoint)) && weakMap.get(endpoint) < 5) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+module.exports = { queryAPI, weakMap };
