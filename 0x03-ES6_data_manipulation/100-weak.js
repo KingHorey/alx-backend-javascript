9561b22c83d28e4d6015a293561b4d68597a3cb2
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-06-04 17:35:43 +0100

    change conditional syntax

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,16 @@
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-    if (endpoint === undefined)
-        return;
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+    weakMap.set(endpoint, 1);
+    return;
   }
-  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 5)) {
+  if ((weakMap.has(endpoint)) && (weakMap.get(endpoint) < 4)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+
   } else {
     throw new Error('Endpoint load is high');
   }
 }
-
+// console.log(weakMap)
 module.exports = { queryAPI, weakMap };
