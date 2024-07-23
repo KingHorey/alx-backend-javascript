45988b3a98d0f6e5a72508d0c79bb02d56467570
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-23 11:49:25 +0100

    receive input and exit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,18 @@
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   let input = process.stdin.read();
 
   if (input !== null) {
     input = input.trim();
-    console.log('Your name is:', input);
+    process.stdout.write(`Your name is: ${input}\n`);
+    process.exit();
   }
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
+  process.exit(0);
 });
