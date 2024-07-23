1d3cec0859d2c56a266c952c1360e44650f10671
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-23 11:28:03 +0100

    receive input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) {
+    console.log('Your name is: ', input);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
