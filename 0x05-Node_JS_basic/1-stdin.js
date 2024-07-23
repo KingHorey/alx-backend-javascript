26ee3026c2c18a4ae27eeb02ac233959507bd850
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-23 12:26:58 +0100

    modify process syntax to properly handle instances where command is run either interactively or data is piped

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,23 @@
 process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+let input = '';
+if (process.stdin.isTTY) {
+  process.stdin.on('readable', () => {
+    let input = process.stdin.read();
+    if (input !== null) {
+      input = input.trim();
+      process.stdout.write(`Your name is: ${input}\n`);
+    }
+  });
+} else {
+  process.stdin.on('data', (chunk) => {
+    input += chunk;
+  });
 
-process.stdin.on('readable', () => {
-  let input = process.stdin.read();
-
-  if (input !== null) {
+  process.stdin.on('end', () => {
     input = input.trim();
     process.stdout.write(`Your name is: ${input}\n`);
-    process.exit();
-  }
-});
-
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
-  process.exit(0);
-});
+    process.stdout.write('This important software is now closing\n');
+  });
+}
