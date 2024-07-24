cfadd9c6424c03e904e3d9ae261c38ebba1ab787
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-24 03:06:57 +0100

    readfile sync

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    const newArray = data.trim().split('\n').slice(1);
+    const cs = [];
+    const swe = [];
+    newArray.filter((item) => item.length > 4);
+    newArray.map((item) => {
+      const csSearch = item.search('CS');
+      if (csSearch !== -1) {
+        const result = /\w*/.exec(item);
+        return cs.push(` ${result[0]}`);
+      }
+      const sweSearch = item.search('SWE');
+      if (sweSearch !== -1) {
+        const result = /\w*/.exec(item);
+        return swe.push(` ${result[0]}`);
+      }
+      return item;
+    });
+    console.log(`Number of students: ${cs.length + swe.length}`);
+    console.log(
+      `Number of students in CS: ${cs.length}. List: ${cs.join(',')}`,
+    );
+    console.log(
+      `Number of students in CS: ${swe.length}. List: ${swe.join(',')}`,
+    );
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
