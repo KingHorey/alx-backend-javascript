3b4c30ca3ddbbd026aa4d8a3ff3249d35ffa8052
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-24 03:55:03 +0100

    create HTTP server with http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const httpListener = (req, res) => {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+};
+
+const port = 1245;
+const host = 'localhost';
+
+const server = http.createServer(httpListener);
+server.listen(port, host, () => {
+  console.log(`server running on ${port}`);
+});
