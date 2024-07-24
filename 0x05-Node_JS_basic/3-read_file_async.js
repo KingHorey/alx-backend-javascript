56cd7762f73130591796526489a28eec76d19c80
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-24 03:40:32 +0100

    async read

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const data = fs.readFile(path, 'utf8', (err, data) => {
+      if (err || !data) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const newArray = data.trim().split('\n').slice(1);
+      const cs = [];
+      const swe = [];
+      newArray.filter((item) => item.length > 4);
+      newArray.forEach((item) => {
+        const csSearch = item.search('CS');
+        if (csSearch !== -1) {
+          const result = /\w*/.exec(item);
+          cs.push(` ${result[0]}`);
+        }
+        const sweSearch = item.search('SWE');
+        if (sweSearch !== -1) {
+          const result = /\w*/.exec(item);
+          swe.push(` ${result[0]}`);
+        }
+      });
+      console.log(`Number of students: ${cs.length + swe.length}`);
+      console.log(
+        `Number of students in CS: ${cs.length}. List:${cs.join(',')}`,
+      );
+      console.log(
+        `Number of students in SWE: ${swe.length}. List:${swe.join(',')}`,
+      );
+      resolve('Done');
+    });
+  });
+}
+
+module.exports = countStudents;
